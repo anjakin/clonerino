9eb30715791dcd9f958d55dbfb80ae1991b5385f
anjakin <amileticku1@etf.unsa.ba>
2016-11-18 14:38:26 +0100

    fixed repeating code

--- Files/ajaxScript.js
+++ Files/ajaxScript.js
@@ -1,7 +1,8 @@
 function ajaxSwitch(newContent) {
-    
+    /*
     var ajax = new XMLHttpRequest();
-
+    var res = encodeURIComponent(newContent);
+    
     ajax.onreadystatechange = function() {
 
         if (ajax.readyState == 4 && ajax.status == 200)
@@ -11,6 +12,9 @@ function ajaxSwitch(newContent) {
             document.getElementById("innercontent").innerHTML = "Greska: nepoznat URL";
     }
 
-    ajax.open("GET", newContent, true);
+    ajax.open("GET", res, true);
     ajax.send();
+    */
+    
+    document.getElementById("innercontent").innerHTML = "heck this";
 }
--- Files/galleryScript.js
+++ Files/galleryScript.js
@@ -1,18 +1,40 @@
-var modal = document.getElementById("modalpic");
-
-var img = document.getElementById("firstimage");
-var modalImg = document.getElementById("img1");
-var captionText = document.getElementById("caption");
-img.onclick = function() {
+function modalThing(index) {
     
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() { 
+    var modal = document.getElementById("modalid");
+    var img = document.getElementsByClassName("images")[index];
+    var modalImg = document.getElementById("img");
+    var captionText = document.getElementById("caption");
     
-  modal.style.display = "none";
+    img.onclick = function() {
+        
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    var span = document.getElementById("close");
+
+    span.onclick = function() { 
+        
+        modal.style.display = "none";
+    }
+    
+    document.onkeydown = function(event) {
+    
+        event = event || window.event;
+        var isEscape = false;
+        if ("key" in event) {
+
+            isEscape = (event.key == "Escape" || event.key == "Esc");
+        } 
+        else {
+
+            isEscape = (event.keyCode == 27);
+        }
+
+        if (isEscape) {
+
+            modal.style.display = "none";
+        }
+    }
 }
